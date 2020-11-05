368b5840f35bee24559df415d1459c79b1ca1d11
JonasWelvaert <jonas.welvaert@gmail.com>
2020-11-05 18:38:10 +0100

    some UI

--- src/barowner/BarOwner.java
+++ src/barowner/BarOwner.java
@@ -1,10 +1,27 @@
 package barowner;
 
-public class BarOwner {
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class BarOwner extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("BarOwner.fxml"));
+	    
+        Scene scene = new Scene(root, 300, 275);
+    
+        primaryStage.setTitle("Bar owner's application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
---
+++ src/barowner/BarOwnerController.java
@@ -0,0 +1,10 @@
+package barowner;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class BarOwnerController {
+
+	@FXML 
+	private Button enrollContactTracingButton;
+}

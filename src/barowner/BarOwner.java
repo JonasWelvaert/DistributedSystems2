55945a31b5403232267e4ffd4d47aae0d87be756
JonasWelvaert <jonas.welvaert@gmail.com>
2020-11-06 12:06:51 +0100

    Some input/output

--- src/barowner/BarOwner.java
+++ src/barowner/BarOwner.java
@@ -1,6 +1,11 @@
 package barowner;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,19 +14,72 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class BarOwner extends Application {
+	private static String fileName;
+	private static String barName;
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("BarOwner.fxml"));
-	    
-        Scene scene = new Scene(root, 300, 275);
-    
-        primaryStage.setTitle("Bar owner's application");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		Parent root = FXMLLoader.load(getClass().getResource("/barowner/BarOwner.fxml"));
+
+		Scene scene = new Scene(root, 380, 360);
+
+		primaryStage.setTitle("Bar owner's application");
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
+		String barName = "";
+		if (args.length == 0) {
+			Scanner scanner = new Scanner(System.in);
+			while (barName.equals("")) {
+				System.out.println("Give a name for your business: ");
+				barName = scanner.nextLine();
+			}
+			scanner.close();
+		} else {
+			barName = String.join(" ", args);
+			System.out.println(barName);
+			if (barName.equals("")) {
+				System.out.println("Fill in a good business name.");
+				System.exit(1);
+			}
+		}
+		fileName = System.getProperty("user.home") + "/.contacttracing/" + barName + ".csv";
+		try {
+			Scanner scanner = new Scanner(new File(fileName));
+			while (scanner.hasNextLine()) {
+				BarOwner.barName = scanner.nextLine();
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("FileNotFoundException in BarOwner.main()");
+			BarOwner.setBarName(barName);
+		}
+
 		launch(args);
+	}
+
+	public static String getBarName() {
+		return barName;
+	}
+
+	public static void setBarName(String barName) {
+		File dir = new File(System.getProperty("user.home") + "/.contacttracing/");
+		if (!dir.exists()) {
+			dir.mkdir();
+		}
+		File file = new File(fileName);
+		FileWriter fw;
+		try {
+			file.createNewFile();
+			fw = new FileWriter(file);
+			fw.write(barName);
+			fw.close();
+			BarOwner.barName = barName;
+		} catch (IOException e) {
+			System.out.println("IOException in BarOwner.setBarName():second try");
+		}
 	}
 }
--- src/barowner/BarOwnerController.java
+++ src/barowner/BarOwnerController.java
@@ -1,10 +1,59 @@
 package barowner;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 
 public class BarOwnerController {
 
-	@FXML 
-	private Button enrollContactTracingButton;
+	@FXML
+	private TextField horecaName;
+
+	@FXML
+	private Label bitstream_label;
+
+	@FXML
+	private Label bitstream;
+
+	@FXML
+	private Button copyToClipboard;
+
+	@FXML
+	private Label proofOfRegistration_label;
+
+	@FXML
+	private Canvas proofOfRegistration;
+
+	@FXML
+	private void initialize() {
+		setEnrolledScene();
+		copyToClipboard.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				Clipboard clipboard = Clipboard.getSystemClipboard();
+				ClipboardContent content = new ClipboardContent();
+				content.putString(bitstream.getText());
+				clipboard.setContent(content);
+			}
+		});
+	}
+
+	private void setEnrolledScene() {
+		horecaName.setEditable(false);
+		horecaName.setText(BarOwner.getBarName());
+		bitstream_label.setVisible(true);
+		// TODO change this bitstream
+		bitstream.setVisible(true);
+		copyToClipboard.setVisible(true);
+		proofOfRegistration_label.setVisible(true);
+		proofOfRegistration.setVisible(true);
+		// TODO change this figure.
+		proofOfRegistration.getGraphicsContext2D().fillOval(10, 60, 30, 30);
+	}
 }
